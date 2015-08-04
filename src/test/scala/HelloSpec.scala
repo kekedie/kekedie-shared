3f8ba52fe9cd0fad247eac62436ef9cfba611a41
Han Sangwon <han_sangwon@cyberagent.co.jp>
2015-08-04 16:34:13 +0900

    test comment

--- src/test/scala/HelloSpec.scala
+++ src/test/scala/HelloSpec.scala
@@ -3,6 +3,6 @@ import org.scalatest._
 class HelloSpec extends FlatSpec with Matchers {
   "Hello" should "have tests" in {
     true should === (true)
-    true should !== (false)
+    // true should !== (false)
   }
 }
